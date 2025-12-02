b07928c7d338592933420d786a6b84392a0623f9
Ubuntu <atambling@Test-VM.gceexixvor0ehlvuscskvlrsjg.ux.internal.cloudapp.net>
2025-12-02 15:57:56 +0000

    updated matchup.py

--- espn_api/football/matchup.py
+++ espn_api/football/matchup.py
@@ -4,18 +4,20 @@ class Matchup(object):
     '''Creates Matchup instance'''
     def __init__(self, data):
         self.matchup_type = data.get('playoffTierType', 'NONE')
+        self.week = data.get('matchupPeriodId')
         self.is_playoff = self.matchup_type != 'NONE'
         (self._home_team_id, self.home_score) = self._fetch_matchup_info(data, 'home')
         (self._away_team_id, self.away_score) = self._fetch_matchup_info(data, 'away')
-        self.home_team: Team
-        self.away_team: Team
+        self.home_team: Team | None = None
+        self.away_team: Team | None = None
 
     def __repr__(self):
-        if hasattr(self, 'away_team'):
-            return f'Matchup({self.home_team}, {self.away_team})'
-        else:
-            return f'Matchup({self.home_team}, N/A)'
-            
+        home = self.home_team if self.home_team is not None else self._home_team_id
+        away = self.away_team if self.away_team is not None else self._away_team_id
+
+        if self.week is not None:
+            return f"Matchup(week={self.week}, home={home}, away={away})"
+        return f"Matchup(home={home}, away={away})"
 
     def _fetch_matchup_info(self, data, team):
         '''Fetch info for matchup'''
